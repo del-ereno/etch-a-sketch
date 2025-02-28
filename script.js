de44db87580bb245b568f4c2508b53058a592c36
del-ereno <delereno2@gmail.com>
2025-02-28 22:18:04 +1300

    created flexbox grid

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const container = document.querySelector("#container");
-for (let i = 1; i <= 16; i++){
-    const div = document.createElement("div");
-    div.style.padding = "50px";
-    div.classList.add(String("div" + i));
-    container.appendChild(div);
+//const container = document.querySelector("#container");
+const columns = document.querySelectorAll("#column");
+columns.forEach((column) => {
+    var row = document.createElement("div");
+    column.appendChild(row);
+    for (let i = 1; i <= 4; i++){
+        const div = document.createElement("div");
+        div.style.padding = "50px";
+        div.classList.add("gridElement");
+        row.appendChild(div);
+    }
+});
+
+
+/** 
+for (let c = 1; c <= 4; i++){
+
+    for (let r = 1; r <= 4; i++){
+        const div = document.createElement("div");
+        div.style.padding = "50px";
+        div.classList.add(String("div"));
+        eval().appendChild(div);
+    }
 }
+*/
